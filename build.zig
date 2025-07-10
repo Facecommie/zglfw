25aafd0e78d1c4e07ee5895b8e87e764ff4f6d90
Nasser West <nasserwest11@gmail.com>
2025-07-10 18:03:03 +0100

    Added lib

--- build.zig
+++ build.zig
@@ -4,27 +4,10 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const glfw_mod = b.addModule("glfw", .{
+    _ = b.addModule("root", .{
         .root_source_file = b.path("src/glfw.zig"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
-    glfw_mod.linkSystemLibrary("glfw", .{});
-
-    const exe = b.addExecutable(.{
-        .name = "sample",
-        .root_source_file = b.path("src/sample.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.root_module.addImport("glfw", glfw_mod);
-
-    const install_exe = b.addInstallArtifact(exe, .{});
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(&install_exe.step);
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 }
